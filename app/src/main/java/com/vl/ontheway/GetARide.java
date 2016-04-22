84b5a0548215542939ee0ae193db58480bde1c37
ragamalika <sai.990880@gmail.com>
2016-04-22 16:49:26 +0530

    Added click action-Ragam

--- app/src/main/java/com/vl/ontheway/GetARide.java
+++ app/src/main/java/com/vl/ontheway/GetARide.java
@@ -1,65 +1,33 @@
 package com.vl.ontheway;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.TextView;
 
-public class GetARide extends AppCompatActivity {
+import com.vl.ontheway.components.CustomTextView;
 
-    private TextView departuretime;
-    private TextView location;
+public class GetARide extends AppCompatActivity implements View.OnClickListener {
+
+    private CustomTextView departuretime;
+    private CustomTextView location;
+    private CustomTextView confirmBtn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_aride);
-        getAcarAlert();
-
     }
 
-    public void getAcarAlert(){
-        final Dialog dialog = new Dialog(GetARide.this);
-
-        LayoutInflater inflater = GetARide.this.getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.layout_car_pulling, null);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setContentView(dialogView);
-        Button mOwnACar=(Button)findViewById(R.id.BT_OwnCar);
-        Button mGroupCar=(Button)findViewById(R.id.BT_GroupCar);
-
-
-
-        mOwnACar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //// TODO: 4/22/2016  server hit to get push notification with car details
-
-                dialog.dismiss();
-            }
-        });
-        mGroupCar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_get_aride, menu);
-        departuretime = (TextView) findViewById(R.id.depTime);
-        location = (TextView) findViewById(R.id.location);
+        departuretime = (CustomTextView) findViewById(R.id.depTime);
+        location = (CustomTextView) findViewById(R.id.location);
+        confirmBtn = (CustomTextView) findViewById(R.id.confirmText);
+        confirmBtn.setOnClickListener(this);
         return true;
     }
 
@@ -81,5 +49,12 @@ public class GetARide extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.confirmText:
+        }
     }
 }
